3af2b40e0b6a05c05b249b97d033993a94c4bdeb
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2021-06-03 23:47:12 +0530

    Quick Sort

---
+++ src/AlgoAgain/DAY12/quickSort.java
@@ -0,0 +1,74 @@
+package AlgoAgain.DAY12;
+
+// Java implementation of QuickSort
+
+public class quickSort {
+
+    // A utility function to swap two elements
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high)
+    {
+
+        // pivot
+        int pivot = arr[high];
+
+        int i = (low - 1);
+
+        for(int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    /* The main function that implements QuickSort
+            arr[] --> Array to be sorted,
+            low --> Starting index,
+            high --> Ending index
+    */
+    static void quick_Sort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+
+            int pi = partition(arr, low, high);
+
+            quick_Sort(arr, low, pi - 1);
+            quick_Sort(arr, pi + 1, high);
+        }
+    }
+
+    // Function to print an array
+    static void printArray(int[] arr, int size)
+    {
+        for(int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int[] arr = { 10, 7, 8, 3, 4, 1, 5 };
+//        int[] arr = { 1, 3, 4, 5, 7, 8, 10 }; i = 2 n .s = i+1 = 3
+        int n = arr.length;
+
+        quick_Sort(arr, 0, n - 1);
+        System.out.println("Sorted array: ");
+        printArray(arr, n);
+    }
+}
+
+// This code is contributed by Ayush Choudhary
--- src/AlgoAgain/DAY7/FindingMinimunLengthUnseratedArrayThenSort.java
+++ src/AlgoAgain/DAY7/FindingMinimunLengthUnseratedArrayThenSort.java
@@ -4,15 +4,14 @@ import java.util.Arrays;
 
 public class FindingMinimunLengthUnseratedArrayThenSort {
     public static void main(String[] args) {
-        int[] arr = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+        int[] arr = {10, 12, 20, 30, 25, 40, 6, 32, 31, 35, 50, 60};
         int startIndex = 0, endIndex = 0;
         for (int i = 0; i < arr.length; i++) {
 
-            int min = i;
             boolean flag = false;
             for (int j = i + 1; j < arr.length; j++) {
                 // 22 < 65
-                if (arr[j] < arr[min]) {
+                if (arr[j] < arr[i]) {
                     startIndex = i;
                     flag = true;
                     break;
@@ -24,11 +23,10 @@ public class FindingMinimunLengthUnseratedArrayThenSort {
 
         for (int i = arr.length - 1; i > startIndex; i--) {
 
-            int max = i;
             boolean flag = false;
             for (int j = i - 1; j > startIndex; j--) {
                 // 22 < 65
-                if (arr[j] > arr[max]) {
+                if (arr[j] > arr[i]) {
                     endIndex = i;
                     flag = true;
                     break;
@@ -38,16 +36,16 @@ public class FindingMinimunLengthUnseratedArrayThenSort {
                 break;
         }
 
-        System.out.println(endIndex + " " + startIndex);
+        System.out.println("Strat Index = " + startIndex + " end Index = " + endIndex);
+        System.out.println("Length = "+ (endIndex - startIndex));
 
-        int[] unsortedArr = new int[endIndex-startIndex+1];
+        int[] unsortedArr = new int[endIndex - startIndex + 1];
 
         int temp = startIndex;
-        for (int i =0; i< unsortedArr.length;i++){
+        for (int i = 0; i < unsortedArr.length; i++) {
             unsortedArr[i] = arr[temp];
             temp++;
         }
-
 
 
         System.out.println(Arrays.toString(unsortedArr));
@@ -56,11 +54,12 @@ public class FindingMinimunLengthUnseratedArrayThenSort {
 
         System.out.println(Arrays.toString(unsortedArr));
 
-        for (int i = 0; i < unsortedArr.length;i++){
+        for (int i = 0; i < unsortedArr.length; i++) {
             arr[startIndex] = unsortedArr[i];
             startIndex++;
         }
-        System.out.println("The length of minimum unsorted arr is "+unsortedArr.length);
+        System.out.println("The length of minimum unsorted arr is " + unsortedArr.length);
+
         System.out.println(Arrays.toString(arr));
     }
 }
